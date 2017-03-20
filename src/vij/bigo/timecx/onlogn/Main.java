87bcf7dc31974016d3e41fa1bc947fc66eb9e5ec
Vijay <vijayaraghavan.varadarajan@gmail.com>
2017-03-20 22:49:11 +0800

    [vvijay] Refactored onlogn.

--- src/vij/bigo/timecx/onlogn/Main.java
+++ src/vij/bigo/timecx/onlogn/Main.java
@@ -4,6 +4,7 @@ import vij.bigo.timecx.BigOTimeCxUtil;
 import vij.bigo.timecx.onsq.BubbleSort;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -24,32 +25,43 @@ public class Main {
         Main bigOnLogn = new Main();
 
         //Create array of different sizes
-        List<int[]> mySortableArraysLst = new ArrayList<>();
-        bigOnLogn.createIntArrays(mySortableArraysLst);
+        List<int[]> arraysToBubbleSortLst = new ArrayList<>();
+        bigOnLogn.createIntArrays(arraysToBubbleSortLst);
 
-        //Sort the arrays
-        for (int[] sortableArray : mySortableArraysLst) {
+        List<int[]> arraysToQuickSortLst = new ArrayList<>();
+        bigOnLogn.createIntArrays(arraysToQuickSortLst);
+
+        //Sort the arrays - With Comparison on Bubble and Quick Sort
+        int sortableArrayIndex = 0;
+        int totalArraysToSort = arraysToQuickSortLst.size();
+        while (sortableArrayIndex < totalArraysToSort) {
 
             System.out.println();
-            int sortableArraySize = sortableArray.length;
+            int sortableArraySize = arraysToBubbleSortLst.get(sortableArrayIndex).length;
 
-            BubbleSort bubbSrt = new BubbleSort(sortableArray);
+            BubbleSort bubbSrt = new BubbleSort(arraysToBubbleSortLst.get(sortableArrayIndex));
             startTime = System.currentTimeMillis();
             bubbSrt.sort();
             endTime = System.currentTimeMillis();
             System.out.println("ArraySize[" + sortableArraySize + "] TimeTaken to BUBBLE SORT[" + (endTime - startTime) + " ms]");
 
-            QuickSort qSrt = new QuickSort(sortableArray);
+            QuickSort qSrt = new QuickSort(arraysToQuickSortLst.get(sortableArrayIndex));
             startTime = System.currentTimeMillis();
             qSrt.sort(0, sortableArraySize-1);
             endTime = System.currentTimeMillis();
+
             System.out.println("ArraySize[" + sortableArraySize + "] TimeTaken to QUICK SORT[" + (endTime - startTime) + " ms]");
+
+//            System.out.println("Bubble Sorted Array: " + Arrays.toString(arraysToBubbleSortLst.get(sortableArrayIndex)));
+//            System.out.println("Quick Sorted Array: " + Arrays.toString(arraysToQuickSortLst.get(sortableArrayIndex)));
+
+            sortableArrayIndex++;
         }
     }
 
     private void createIntArrays(List<int[]> myIntArrayLst) {
 
-        int[] arraySizes = {10000, 20000, 30000, 40000, 50000};
+        int[] arraySizes = {1000, 2000, 3000, 4000, 5000};
         for (int arraySize : arraySizes) {
 
             myIntArrayLst.add(BigOTimeCxUtil.createRandomIntArray(arraySize));
